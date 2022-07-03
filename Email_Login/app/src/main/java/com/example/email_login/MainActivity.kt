c74a2628ce2faedcee8e47ff8f9f7275999da915
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2022-07-04 03:18:30 +0530

    Email Login

---
+++ Email_Login/app/src/main/java/com/example/email_login/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.email_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingstuff.loginandsignup.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
