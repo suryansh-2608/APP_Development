b4a106821ded5a0e53f5e4d070370df2e7e49842
Suryansh Bhardwaj <84323661+suryansh-2608@users.noreply.github.com>
2023-03-21 19:36:04 +0530

    Added dynamic Dimensions
    removed the hardcode for dimensions and added dynamic dimensions

--- Food_deliv/lib/home/food_page_body.dart
+++ Food_deliv/lib/home/food_page_body.dart
@@ -1,6 +1,8 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_deliv/utils/colors.dart';
+import 'package:food_deliv/utils/dimensions.dart';
 import 'package:food_deliv/widgets/big_text.dart';
 import 'package:food_deliv/widgets/icon_and_text_widget.dart';
 import 'package:food_deliv/widgets/small_text.dart';
@@ -13,83 +15,157 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = Dimensions.pageViewContainer;
+
+  @override
+  void initState(){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    PageController pageController = PageController(viewportFraction: 0.85);
-    return Container(
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context, position){
-        return _buildPageItem(position);
-      }),
+
+    return Column(
+      children: [
+        Container(
+          height: Dimensions.pageView,
+          child: PageView.builder(
+            controller: pageController,
+              itemCount: 5,
+              itemBuilder: (context, position){
+            return _buildPageItem(position);
+          }),
+        ),
+        new DotsIndicator(
+            dotsCount: 5,
+            position: _currPageValue,
+        decorator: DotsDecorator(
+          activeColor: AppColors.mainColor,
+          size: const Size.square(9.0),
+          activeSize: const Size(18.0, 9.0),
+          activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+        ),
+        )
+      ],
     );
   }
   Widget _buildPageItem(int index){
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven?Color(0xFF69c5df):Color(0xFF92294cc),
-              image: DecorationImage(fit:BoxFit.cover, image: AssetImage("assets/image/food0.png"))
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+    Matrix4 matrix = new Matrix4.identity();
+    if(index == _currPageValue.floor()){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    }else if(index == _currPageValue.floor()+1){
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else if(index == _currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else{
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2,1);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: Dimensions.pageViewContainer,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: Colors.white
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
-              child:Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chinese Side"),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor)),
-
-                      ),
-                      SizedBox(width: 10,),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "1767"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "Comments")
-                    ],
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    children: [
-                      IconAndTextWidget(icon: Icons.circle_sharp,
-                          text: "Normal",
-                          iconColor: AppColors.iconColor1),
-                      SizedBox(width: 15,),
-                      IconAndTextWidget(icon: Icons.location_on,
-                          text: "1.7 Km",
-                          iconColor: AppColors.mainColor),
-                      SizedBox(width: 15,),
-                      IconAndTextWidget(icon: Icons.access_time,
-                          text: "Normal",
-                          iconColor: AppColors.iconColor2),
-                    ],
-                  )
-                ],
-              ),
+                color: index.isEven?Color(0xFF69c5df):Color(0xFF92294cc),
+                image: DecorationImage(fit:BoxFit.cover, image: AssetImage("assets/image/food0.png"))
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: Dimensions.pageViewTextContainer,
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0)
+                  ),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(5, 0)
+                  )
+                ]
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(height: Dimensions.height10),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor)),
+
+                        ),
+                        SizedBox(width: 10,),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1767"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "Comments")
+                      ],
+                    ),
+                    SizedBox(height: Dimensions.height20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1),
+                        SizedBox(width: 15,),
+                        IconAndTextWidget(icon: Icons.location_on,
+                            text: "1.7 Km",
+                            iconColor: AppColors.mainColor),
+                        SizedBox(width: 15,),
+                        IconAndTextWidget(icon: Icons.access_time,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor2),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ Food_deliv/lib/utils/dimensions.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class Dimensions{
+    static double screenHeight = Get.context!.height;
+    static double screenWidth = Get.context!.width;
+    static double pageView = screenHeight/2.74;
+    static double pageViewContainer = screenHeight/4;
+    static double pageViewTextContainer = screenHeight/7.2;
+    static double height10 = screenHeight/91.2;
+    static double height15 = screenHeight/61;
+    static double height20 = screenHeight/45.6;
+    static double height30 = screenHeight/30;
+    static double font20 = screenHeight/45.6;
+    static double radius20 =screenHeight/45.6;
+    static double radius30 = screenHeight/30;
+    static double radius15 =screenHeight/61;
+    static double width10 = screenHeight/41.2;
+    static double width15 = screenHeight/28;
+    static double width20 = screenHeight/20;
+    static double width30 = screenHeight/14;
+
+
+
+
+
+}
